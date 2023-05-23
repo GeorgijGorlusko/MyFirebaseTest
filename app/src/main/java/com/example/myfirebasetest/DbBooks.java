1af463712a1008fc006979cfbe9c72378681238b
Igor Filchev <90631197+filchev-igor@users.noreply.github.com>
2023-05-23 22:00:03 +0300

    Revert "IssueBook was elaborated (#1)"
    This reverts commit 4db27bfc55f208c55871a8dafa4de012d60ccb1d.

--- app/src/main/java/com/example/myfirebasetest/DbBooks.java
+++
@@ -1,9 +0,0 @@
-package com.example.myfirebasetest;
-
-public class DbBooks {
-    public String title;
-    public int available;
-    public int total;
-    public int id;
-    public String type;
-}
